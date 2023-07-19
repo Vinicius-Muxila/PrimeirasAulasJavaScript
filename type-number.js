d38b752af82df1f25b098a776aefaa89b5f27f04
Vinicius-Muxila <viniciuss.nuness@gmail.com>
2023-07-19 16:26:39 -0300

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,15 @@
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica)
+
+const n1 = 3.5;
+const n2 = 2.3;
+
+const soma = n1 + n2;
+
+console.log(soma)
---
+++ type-string.js
@@ -0,0 +1,16 @@
+const texto1 = "Olá, mundo!"
+const texto2 = 'Olá, mundo'
+
+//console.log(texto1)
+//console.log(texto2)
+
+const texto = "Meu nome é "
+const nome = "Vinícius"
+
+console.log(texto + nome)
+
+
+const senha = "minhaSenha123"
+//console.log(senha.length) // 13 caracteres
+
+
